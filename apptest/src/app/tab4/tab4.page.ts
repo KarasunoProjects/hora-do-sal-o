0ba81109bcd06f309aa38bb146872705cc267c0d
Prantes <63598185+Prantes@users.noreply.github.com>
2020-10-20 12:09:16 -0300

    login com conta google
    Ajustes com login usando a conta google, página de perfil estilizada e recebendo dados da conta do usuario

--- apptest/src/app/tab4/tab4.page.ts
+++ apptest/src/app/tab4/tab4.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+// 1) importar classes de autenticação
+import {AngularFireAuth, AngularFireAuthModule} from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+  constructor(
+    // 2)criando  objeto de autenticação
+  public afAuth: AngularFireAuth)
+   { }
 
   ngOnInit() {
+  }
+   // 3) Metodode Login
+   login(){
+    this.afAuth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+  // 4)Metodo de logout
+  logout(){
+    this.afAuth.signOut();
   }
 
 }
